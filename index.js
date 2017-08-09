9dc690aced07225c491eae07f8f2de5e3efa7035
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-09 13:35:50 +0530

    Server created in hapi

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 const hapi = require('hapi')
 const inert = require('inert')
 
+const server = new hapi.Server();
+server.connection({
+    port: 80,
+    routes: {
+        files: {
+            relativeTo: '../PS-Timesheet-App'
+        }
+    }
+});
 
+server.register(inert,() => {})
+
+server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+        directory: {
+            path: 'src',
+            index: true,
+            redirectToSlash: true
+        }
+    }
+});
+
+server.start((err) => {
+    if (err)
+        throw err;
+
+    console.log("Server running...");
+})
