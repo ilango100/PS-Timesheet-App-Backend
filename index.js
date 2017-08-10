d091c8ba2d933b91001788cf6969fc7d23506d1c
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-10 12:08:53 +0530

    Database and tables setup finished!

--- index.js
+++ index.js
@@ -6,92 +6,112 @@ let args = process.argv.slice(2);
 let msquser = "";
 let msqpass = "";
 if (args.length < 2) {
-    console.log("Usage:",process.argv[0],process.argv[1],"username password");
-    process.exit(1);
+	console.log("Usage:",process.argv[0],process.argv[1],"username password");
+	process.exit(1);
 } else {
-    msquser = args[0];
-    msqpass = args[1];
+	msquser = args[0];
+	msqpass = args[1];
 }
 
 let dbconn = mysql.createConnection({
-    host: 'localhost',
-    user: msquser,
-    password: msqpass,
+	host: 'localhost',
+	user: msquser,
+	password: msqpass,
 });
 
 //Connect to mysql
 dbconn.connect(err => {
-    if (err) {
-        console.log("Error connecting to db...",err.message);
-        process.exit(1);
-    }
-    console.log("Connected to mysql server successfully...")
+	if (err) {
+		console.log("Error connecting to db...",err.message);
+		process.exit(1);
+	}
+	console.log("Connected to mysql server successfully...")
 });
 
 dbconn.query("show databases",function(err,results,field) {
-    if (err) {
-        console.log("Error querying...",err.message);
-        process.exit(1);
-    }
-    let ourdb = results.find(o => o.Database == 'timesheet');
-    console.log(ourdb);
-    if (ourdb != null && ourdb != undefined && ourdb['Database'] != null) {
-        console.log("Database exists...");
-    } else {
-        console.log("Database doesnot exist, creating...");
-        dbconn.query("create database timesheet",function(er,res,f) {
-            if (err) {
-                console.log("Error creating database...",er)
-                process.exit(1);
-            }
-        });
-    }
-    dbconn.query("use timesheet",function(er,res,f) {
-        if (er) {
-            console.log("Error while using timesheet database...")
-        }
-    })
+	if (err) {
+		console.log("Error querying...",err.message);
+		process.exit(1);
+	}
+	let ourdb = results.find(o => o.Database == 'timesheet');
+	if (ourdb != null && ourdb != undefined && ourdb['Database'] != null) {
+		console.log("Database exists...");
+	} else {
+		console.log("Database doesnot exist, creating...");
+		dbconn.query("create database timesheet",function(er,res,f) {
+			if (err) {
+				console.log("Error creating database...",er.message)
+				process.exit(1);
+			}
+		});
+	}
+	dbconn.query("use timesheet",function(er,res,fl) {
+		if (er) {
+			console.log("Error while using timesheet database...")
+		}
+		//Setup tables
+		dbconn.query("show tables",function(e,r,f) {
+			if (e) {
+				console.log("Error querying tables...",e.message);
+				process.exit(1);
+			}
+			let ourtb = r.find(o => o.Tables_in_timesheet == 'users');
+			if (ourdb != null && ourtb != undefined && ourtb['Tables_in_timesheet'] != null) {
+				console.log("Users table exists...");
+			} else {
+				console.log("Users table doesnot exist, creating...");
+				dbconn.query("create table users (username VARCHAR(20) PRIMARY KEY, password VARCHAR(12),\
+				dob DATE, email VARCHAR(50))",function(f,s,g) {
+					if (f) {
+						console.log("Error creating table...",f.message)
+						process.exit(1);
+					}
+					console.log("Successfully created users table...");
+				});
+			}
+		});
+	})
 });
 
 //Start server
 const server = new hapi.Server();
 server.connection({
-    port: 80,
-    routes: {
-        files: {
-            relativeTo: '../PS-Timesheet-App'
-        }
-    }
+	port: 80,
+	routes: {
+		files: {
+			relativeTo: '../PS-Timesheet-App'
+		}
+	}
 });
 
 server.register(inert,() => {});
 
 server.route({
-    method: 'GET',
-    path: '/{path*}',
-    handler: {
-        directory: {
-            path: 'dist',
-            index: true,
-            redirectToSlash: true
-        }
-    }
+	method: 'GET',
+	path: '/{path*}',
+	handler: {
+		directory: {
+			path: 'dist',
+			index: true,
+			redirectToSlash: true
+		}
+	}
 });
 
 server.route({
-    method: 'POST',
-    path: '/login',
-    handler: function(req,resp) {
-	//Do db operation
-    return resp({
-        status: 'ok',
-    }).type("application/json");
-    }
+	method: 'POST',
+	path: '/login',
+	handler: function(req,resp) {
+		//Do db operation
+		return resp({
+			status: 'ok',
+		}).type("application/json");
+	}
 });
 
 server.start((err) => {
-    if (err)
-        throw err;
+	if (err)
+		throw err;
 
-    console.log("Server running...");
+	console.log("Server running...");
 });
